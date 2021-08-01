2dd68e8e63977790d626cd302f752a999718a5da
Evgeniia <zhe-karimova@yandex.ru>
2021-08-01 19:21:49 +0300

    Add the project

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,16 @@
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.testng.IDataProviderMethod;
+import org.testng.annotations.DataProvider;
+
+
+import static org.testng.Assert.assertEquals;
+
+public class AppTest {
+
+
+
+}
